fea8283b4691b937bef8082ddad759a3d0d77b41
Kaleb Elwert <belak@coded.io>
2020-01-25 15:01:29 -0800

    Initial code commit
    This code comes from the official django tutorial, located at
    https://docs.djangoproject.com/en/3.0/intro/

---
+++ polls/views.py
@@ -0,0 +1,69 @@
+from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
+
+from .models import Choice, Question
+
+
+class QuestionQuerysetMixin:
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet for any non-admins.
+        """
+
+        if self.request.user.is_staff:
+            return Question.objects.all()
+
+        return Question.objects.order_by('-pub_date').filter(pub_date__lte=timezone.now())
+
+
+class IndexView(QuestionQuerysetMixin, generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions.
+        """
+        return super().get_queryset()[:5]
+
+
+class DetailView(QuestionQuerysetMixin, generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(QuestionQuerysetMixin, generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += F('votes') + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
